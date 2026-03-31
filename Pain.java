b4ac71e09d24ebb39030de0ff8a476f151d99b40
KALAIARASI S <kalaiarasi.s.aids29@princedrkvasudevan.com>
2026-03-31 23:47:19 +0530

    Delete Pain.java

--- Pain.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-class Pain{
-    public static void main(String[]args){
-    Scanner scan = new Scanner(System.in);
-    int n = scan.nextInt();
-    int fact = factorial(n);
-    System.out.print(fact);
